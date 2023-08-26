15ea9086e193a05388298e070963d01104575295
sejalg1019 <73032519+sejalg1019@users.noreply.github.com>
2023-08-26 10:47:53 -0400

    Add files via upload

---
+++ Throw.js
@@ -0,0 +1,60 @@
+AFRAME.registerComponent("bowling-balls",{
+    init: function(){
+        this.throwBall()
+    },
+
+    throwBall: function(){
+        window.addEventListener("keydown", (e) => {
+            if(e.key === "z"){
+                var ball = document.createElement("a-entity")
+
+                ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf")
+                ball.setAttribute("scale", {x: 3, y: 3, z: 3})
+
+                var cam = document.querySelector("#camera")
+                pos = cam.getAttribute("position")
+
+                ball.setAttribute("position", {
+                    x: pos.x,
+                    y: pos.y-0.2,
+                    z: pos.z
+                })
+
+                ball.setAttribute("dynamic-body", {
+                    shape: "sphere",
+                    mass: "0"
+                })
+                
+                var camera = document.querySelector("#camera").object3D
+
+                var direction = new THREE.Vector3()
+                camera.getWorldDirection(direction)
+
+                ball.setAttribute("velocity", direction.multiplyScalar(-10))
+
+                var scene = document.querySelector("#scene")
+
+                ball.addEventListener("collide", this.removeBall)
+                scene.appendChild(ball)
+            }
+        })
+    },
+    removeBall: function(e) {
+        console.log(e.detail.target.el)
+        console.log(e.detail.body.el)
+
+        var element = e.detail.target.el
+        var elementHit = e.detail.body.el
+
+        if(elementHit.id.includes("pin")){
+            var impulse = new CANNON.Vec3(0, 1, -15)
+            var worldPoint = new CANNON.Vec3().copy(elementHit.getAttribute("position"))
+
+            elementHit.body.applyForce(impulse, worldPoint)
+            element.removeEventListener("collide", this.removeBall)
+
+            var scene = document.querySelector("#scene")
+            scene.removeChild(element)
+        }
+    }
+})
